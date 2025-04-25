792f3ddbc310dde39631091c0733e9dc2f822d87
Nasser Al Nasser <113646760+Nascaral@users.noreply.github.com>
2025-04-24 23:15:37 -0700

    Update Rendezvous.java

--- nachos/threads/Rendezvous.java
+++ nachos/threads/Rendezvous.java
@@ -1,40 +1,58 @@
 package nachos.threads;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+
 import nachos.machine.*;
 
+
 public class Rendezvous {
-   private int[] items;
-    private boolean[] hasItem;
-    private Lock lock;
-    private Condition[] conditions;
-    
-    public Rendezvous(int n) {
-        items = new int[n];
-        hasItem = new boolean[n];
-        lock = new Lock();
-        conditions = new Condition[n];
-        for( int i =0; i < n; i++){
-            conditions[i] = new Condition(lock);
+
+  
+    private static class Slot {
+        final int     tag;
+        int           item;      
+        final KThread thread;
+
+        Slot(int tag, int item, KThread t) {
+            this.tag    = tag;
+            this.item   = item;
+            this.thread = t;
         }
     }
 
-    public int exchange(int myItem, int tag){
+ 
+    public Rendezvous () { 
+
+    public int exchange (int tag, int myItem) {          // FIX: parameter order & return type
         lock.acquire();
 
-        if (hasItem[tag]){
-            int otherItem = items[tag];
-            hasItem[tag] = false;
-            conditions[tag].wake();
-            lock.release();
-            return otherItem;
-        } else {
-            items[tag] = true;
-            conditions[tag].sleep();
-            int otherItem = items[tag];
-            hasItem[tag] = false;
-            lock.release();
-            return otherItem;
-        }
-    }
-}
+        LinkedList<Slot> queue =
+            waiting.computeIfAbsent(tag, k -> new LinkedList<>());
 
+     
+        if (!queue.isEmpty()) {
+            Slot partner = queue.removeFirst();
+            int ret      = partner.item;   // value to return to me
+            partner.item = myItem;         // give mine to partner
+            partner.thread.ready();        // wake partner
+            lock.release();
+            return ret;
+        }
+
+        /* Otherwise, enqueue myself and go to sleep. */
+        Slot me = new Slot(tag, myItem, KThread.currentThread());
+        queue.addLast(me);
+
+        boolean intStatus = Machine.interrupt().disable();
+        lock.release();                    // let a partner in
+        KThread.sleep();                   // block
+        Machine.interrupt().restore(intStatus);
+
+      
+        return me.item;
+    }
+
+    private final Lock                                    lock     = new Lock();
+    private final HashMap<Integer,LinkedList<Slot>>       waiting  = new HashMap<>();
+}
