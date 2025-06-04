6ebc0cd639d4987f237589837e093c3996f54bf3
Al Nasser <nalnasser@ieng6-201.ucsd.edu>
2025-06-03 22:10:48 -0700

    All tasks

--- nachos/vm/VMKernel.java
+++ nachos/vm/VMKernel.java
@@ -4,48 +4,267 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
+import java.util.*;
 
-/**
- * A kernel that can support multiple demand-paging user processes.
- */
 public class VMKernel extends UserKernel {
-	/**
-	 * Allocate a new VM kernel.
-	 */
-	public VMKernel() {
-		super();
-	}
+    private static Lock vmLock;
+    private static HashMap<Integer, VMProcess> processMap;
+    private static InvertedPageTable pageTable;
+    private static OpenFile swapFile;
+    private static int swapPageCounter;
+    private static LinkedList<Integer> freeSwapPages;
+    private static Lock swapLock;
+    private static Condition2 pagesAvailable;
+    private static int clockHand;
+    
+    public VMKernel() {
+        super();
+    }
 
-	/**
-	 * Initialize this kernel.
-	 */
-	public void initialize(String[] args) {
-		super.initialize(args);
-	}
+    public void initialize(String[] args) {
+        super.initialize(args);
+        
+        vmLock = new Lock();
+        processMap = new HashMap<>();
+        pageTable = new InvertedPageTable(Machine.processor().getNumPhysPages());
+        
+        swapFile = ThreadedKernel.fileSystem.open("SWAP_FILE_NACHOS", true);
+        if (swapFile == null) {
+            Lib.debug(dbgVM, "Failed to create swap file");
+            Lib.assertNotReached();
+        }
+        
+        swapPageCounter = 0;
+        freeSwapPages = new LinkedList<>();
+        swapLock = new Lock();
+        pagesAvailable = new Condition2(vmLock);
+        clockHand = 0;
+    }
 
-	/**
-	 * Test this kernel.
-	 */
-	public void selfTest() {
-		super.selfTest();
-	}
+    public void selfTest() {
+        super.selfTest();
+    }
 
-	/**
-	 * Start running user programs.
-	 */
-	public void run() {
-		super.run();
-	}
+    public void run() {
+        super.run();
+    }
 
-	/**
-	 * Terminate this kernel. Never returns.
-	 */
-	public void terminate() {
-		super.terminate();
-	}
+    public void terminate() {
+        if (swapFile != null) {
+            swapFile.close();
+            ThreadedKernel.fileSystem.remove("SWAP_FILE_NACHOS");
+        }
+        super.terminate();
+    }
+    
+    public static void registerProcess(VMProcess process) {
+        vmLock.acquire();
+        processMap.put(process.getPID(), process);
+        vmLock.release();
+    }
+    
+    public static void unregisterProcess(VMProcess process) {
+        vmLock.acquire();
+        processMap.remove(process.getPID());
+        vmLock.release();
+    }
+    
+    public static int allocatePhysicalPage(VMProcess process, int vpn) {
+        vmLock.acquire();
+        
+        int ppn = -1;
+        for (int i = 0; i < pageTable.size(); i++) {
+            if (!pageTable.isOccupied(i)) {
+                ppn = i;
+                pageTable.setEntry(i, process, vpn);
+                break;
+            }
+        }
+        
+        if (ppn == -1) {
+            ppn = evictPage();
+            if (ppn != -1) {
+                pageTable.setEntry(ppn, process, vpn);
+            }
+        }
+        
+        vmLock.release();
+        return ppn;
+    }
+    
+    private static int evictPage() {
+        int numPages = pageTable.size();
+        int startHand = clockHand;
+        
+        while (true) {
+            int currentHand = clockHand;
+            clockHand = (clockHand + 1) % numPages;
+            
+            if (pageTable.isPinned(currentHand)) {
+                if (clockHand == startHand) {
+                    while (pageTable.isPinned(currentHand)) {
+                        pagesAvailable.sleep();
+                        currentHand = clockHand;
+                        clockHand = (clockHand + 1) % numPages;
+                    }
+                }
+                continue;
+            }
+            
+            VMProcessInfo info = pageTable.getProcessInfo(currentHand);
+            if (info == null) continue;
+            
+            VMProcess owner = processMap.get(info.pid);
+            if (owner == null) continue;
+            
+            TranslationEntry te = owner.getPageTableEntry(info.vpn);
+            if (te == null || !te.valid) continue;
+            
+            if (te.used) {
+                te.used = false;
+                if (clockHand == startHand) {
+                    continue;
+                }
+            } else {
+                if (te.dirty) {
+                    int swapPage = allocateSwapPage();
+                    if (swapPage != -1) {
+                        writePageToSwap(currentHand, swapPage);
+                        owner.setSwapPage(info.vpn, swapPage);
+                    }
+                } else if (owner.hasSwapPage(info.vpn)) {
+                } else {
+                }
+                
+                te.valid = false;
+                pageTable.clearEntry(currentHand);
+                return currentHand;
+            }
+        }
+    }
+    
+    public static void pinPage(int ppn) {
+        vmLock.acquire();
+        pageTable.pinPage(ppn);
+        vmLock.release();
+    }
+    
+    public static void unpinPage(int ppn) {
+        vmLock.acquire();
+        pageTable.unpinPage(ppn);
+        pagesAvailable.wakeAll();
+        vmLock.release();
+    }
+    
+    public static int allocateSwapPage() {
+        swapLock.acquire();
+        int swapPage;
+        
+        if (!freeSwapPages.isEmpty()) {
+            swapPage = freeSwapPages.removeFirst();
+        } else {
+            swapPage = swapPageCounter++;
+        }
+        
+        swapLock.release();
+        return swapPage;
+    }
+    
+    public static void freeSwapPage(int swapPage) {
+        swapLock.acquire();
+        freeSwapPages.add(swapPage);
+        swapLock.release();
+    }
+    
+    public static void writePageToSwap(int ppn, int swapPage) {
+        byte[] memory = Machine.processor().getMemory();
+        byte[] buffer = new byte[pageSize];
+        
+        System.arraycopy(memory, ppn * pageSize, buffer, 0, pageSize);
+        
+        int bytesWritten = swapFile.write(swapPage * pageSize, buffer, 0, pageSize);
+        Lib.assertTrue(bytesWritten == pageSize);
+    }
+    
+    public static void readPageFromSwap(int swapPage, int ppn) {
+        byte[] memory = Machine.processor().getMemory();
+        byte[] buffer = new byte[pageSize];
+        
+        int bytesRead = swapFile.read(swapPage * pageSize, buffer, 0, pageSize);
+        Lib.assertTrue(bytesRead == pageSize);
+        
+        System.arraycopy(buffer, 0, memory, ppn * pageSize, pageSize);
+    }
+    
+    public static void zeroFillPage(int ppn) {
+        byte[] memory = Machine.processor().getMemory();
+        Arrays.fill(memory, ppn * pageSize, (ppn + 1) * pageSize, (byte)0);
+    }
+    
+    public static Lock getVMLock() {
+        return vmLock;
+    }
+    
+    public static void freePhysicalPage(int ppn) {
+        vmLock.acquire();
+        pageTable.clearEntry(ppn);
+        vmLock.release();
+    }
+    
+    private static class InvertedPageTable {
+        private VMProcessInfo[] entries;
+        private boolean[] pinned;
+        
+        public InvertedPageTable(int size) {
+            entries = new VMProcessInfo[size];
+            pinned = new boolean[size];
+        }
+        
+        public int size() {
+            return entries.length;
+        }
+        
+        public boolean isOccupied(int ppn) {
+            return entries[ppn] != null;
+        }
+        
+        public void setEntry(int ppn, VMProcess process, int vpn) {
+            entries[ppn] = new VMProcessInfo(process.getPID(), vpn);
+        }
+        
+        public void clearEntry(int ppn) {
+            entries[ppn] = null;
+            pinned[ppn] = false;
+        }
+        
+        public VMProcessInfo getProcessInfo(int ppn) {
+            return entries[ppn];
+        }
+        
+        public void pinPage(int ppn) {
+            pinned[ppn] = true;
+        }
+        
+        public void unpinPage(int ppn) {
+            pinned[ppn] = false;
+        }
+        
+        public boolean isPinned(int ppn) {
+            return pinned[ppn];
+        }
+    }
+    
+    private static class VMProcessInfo {
+        public int pid;
+        public int vpn;
+        
+        public VMProcessInfo(int pid, int vpn) {
+            this.pid = pid;
+            this.vpn = vpn;
+        }
+    }
 
-	// dummy variables to make javac smarter
-	private static VMProcess dummy1 = null;
-
-	private static final char dbgVM = 'v';
+    private static VMProcess dummy1 = null;
+    private static final char dbgVM = 'v';
+    private static final int pageSize = Processor.pageSize;
 }
--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -4,71 +4,279 @@ import nachos.machine.*;
 import nachos.threads.*;
 import nachos.userprog.*;
 import nachos.vm.*;
+import java.util.*;
 
-/**
- * A <tt>UserProcess</tt> that supports demand-paging.
- */
 public class VMProcess extends UserProcess {
-	/**
-	 * Allocate a new process.
-	 */
-	public VMProcess() {
-		super();
-	}
+    private HashMap<Integer, Integer> vpnToSwapPage;
+    private Lock processLock;
+    
+    public VMProcess() {
+        super();
+        vpnToSwapPage = new HashMap<>();
+        processLock = new Lock();
+    }
 
-	/**
-	 * Save the state of this process in preparation for a context switch.
-	 * Called by <tt>UThread.saveState()</tt>.
-	 */
-	public void saveState() {
-		super.saveState();
-	}
+    public void saveState() {
+        super.saveState();
+    }
 
-	/**
-	 * Restore the state of this process after a context switch. Called by
-	 * <tt>UThread.restoreState()</tt>.
-	 */
-	public void restoreState() {
-		super.restoreState();
-	}
+    public void restoreState() {
+        super.restoreState();
+    }
 
-	/**
-	 * Initializes page tables for this process so that the executable can be
-	 * demand-paged.
-	 * 
-	 * @return <tt>true</tt> if successful.
-	 */
-	protected boolean loadSections() {
-		return super.loadSections();
-	}
+    protected boolean loadSections() {
+        pageTable = new TranslationEntry[numPages];
+        for (int i = 0; i < numPages; i++) {
+            pageTable[i] = new TranslationEntry(i, 0, false, false, false, false);
+        }
+        
+        VMKernel.registerProcess(this);
+        return true;
+    }
 
-	/**
-	 * Release any resources allocated by <tt>loadSections()</tt>.
-	 */
-	protected void unloadSections() {
-		super.unloadSections();
-	}
+    protected void unloadSections() {
+        processLock.acquire();
+        
+        for (int i = 0; i < pageTable.length; i++) {
+            if (pageTable[i] != null && pageTable[i].valid) {
+                VMKernel.freePhysicalPage(pageTable[i].ppn);
+            }
+            
+            if (vpnToSwapPage.containsKey(i)) {
+                VMKernel.freeSwapPage(vpnToSwapPage.get(i));
+            }
+        }
+        
+        vpnToSwapPage.clear();
+        processLock.release();
+        
+        VMKernel.unregisterProcess(this);
+    }
 
-	/**
-	 * Handle a user exception. Called by <tt>UserKernel.exceptionHandler()</tt>
-	 * . The <i>cause</i> argument identifies which exception occurred; see the
-	 * <tt>Processor.exceptionZZZ</tt> constants.
-	 * 
-	 * @param cause the user exception that occurred.
-	 */
-	public void handleException(int cause) {
-		Processor processor = Machine.processor();
+    public void handleException(int cause) {
+        Processor processor = Machine.processor();
 
-		switch (cause) {
-		default:
-			super.handleException(cause);
-			break;
-		}
-	}
+        switch (cause) {
+        case Processor.exceptionPageFault:
+            int vaddr = processor.readRegister(Processor.regBadVAddr);
+            handlePageFault(vaddr);
+            break;
+        default:
+            super.handleException(cause);
+            break;
+        }
+    }
+    
+    private void handlePageFault(int vaddr) {
+        int vpn = vaddr / pageSize;
+        
+        if (vpn < 0 || vpn >= pageTable.length) {
+            handleExit(-1);
+            return;
+        }
+        
+        VMKernel.getVMLock().acquire();
+        
+        if (pageTable[vpn].valid) {
+            VMKernel.getVMLock().release();
+            return;
+        }
+        
+        int ppn = VMKernel.allocatePhysicalPage(this, vpn);
+        if (ppn == -1) {
+            VMKernel.getVMLock().release();
+            handleExit(-1);
+            return;
+        }
+        
+        pageTable[vpn].ppn = ppn;
+        
+        processLock.acquire();
+        boolean loaded = false;
+        
+        if (vpnToSwapPage.containsKey(vpn)) {
+            VMKernel.getVMLock().release();
+            
+            int swapPage = vpnToSwapPage.get(vpn);
+            VMKernel.readPageFromSwap(swapPage, ppn);
+            loaded = true;
+            
+            VMKernel.getVMLock().acquire();
+        }
+        
+        if (!loaded) {
+            VMKernel.getVMLock().release();
+            
+            loaded = loadPageFromCoff(vpn, ppn);
+            if (!loaded) {
+                VMKernel.zeroFillPage(ppn);
+            }
+            
+            VMKernel.getVMLock().acquire();
+        }
+        
+        pageTable[vpn].valid = true;
+        pageTable[vpn].used = false;
+        pageTable[vpn].dirty = false;
+        
+        boolean isReadOnly = false;
+        for (int s = 0; s < coff.getNumSections(); s++) {
+            CoffSection section = coff.getSection(s);
+            if (vpn >= section.getFirstVPN() && 
+                vpn < section.getFirstVPN() + section.getLength()) {
+                isReadOnly = section.isReadOnly();
+                break;
+            }
+        }
+        pageTable[vpn].readOnly = isReadOnly;
+        
+        processLock.release();
+        VMKernel.getVMLock().release();
+    }
+    
+    private boolean loadPageFromCoff(int vpn, int ppn) {
+        for (int s = 0; s < coff.getNumSections(); s++) {
+            CoffSection section = coff.getSection(s);
+            int firstVPN = section.getFirstVPN();
+            
+            if (vpn >= firstVPN && vpn < firstVPN + section.getLength()) {
+                section.loadPage(vpn - firstVPN, ppn);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public int readVirtualMemory(int vaddr, byte[] data, int offset, int length) {
+        Lib.assertTrue(offset >= 0 && length >= 0 && offset + length <= data.length);
+        
+        byte[] memory = Machine.processor().getMemory();
+        int amount = 0;
+        
+        while (length > 0 && vaddr < numPages * pageSize) {
+            int vpn = vaddr / pageSize;
+            int addrOffset = vaddr % pageSize;
+            int transfer = Math.min(length, pageSize - addrOffset);
+            
+            if (vpn < 0 || vpn >= pageTable.length) {
+                break;
+            }
+            
+            VMKernel.getVMLock().acquire();
+            
+            if (!pageTable[vpn].valid) {
+                VMKernel.getVMLock().release();
+                handlePageFault(vaddr);
+                VMKernel.getVMLock().acquire();
+                
+                if (!pageTable[vpn].valid) {
+                    VMKernel.getVMLock().release();
+                    break;
+                }
+            }
+            
+            int ppn = pageTable[vpn].ppn;
+            VMKernel.pinPage(ppn);
+            VMKernel.getVMLock().release();
+            
+            int paddr = ppn * pageSize + addrOffset;
+            System.arraycopy(memory, paddr, data, offset, transfer);
+            
+            pageTable[vpn].used = true;
+            
+            VMKernel.unpinPage(ppn);
+            
+            vaddr += transfer;
+            offset += transfer;
+            amount += transfer;
+            length -= transfer;
+        }
+        
+        return amount;
+    }
+    
+    public int writeVirtualMemory(int vaddr, byte[] data, int offset, int length) {
+        Lib.assertTrue(offset >= 0 && length >= 0 && offset + length <= data.length);
+        
+        byte[] memory = Machine.processor().getMemory();
+        int amount = 0;
+        
+        while (length > 0 && vaddr < numPages * pageSize) {
+            int vpn = vaddr / pageSize;
+            int addrOffset = vaddr % pageSize;
+            int transfer = Math.min(length, pageSize - addrOffset);
+            
+            if (vpn < 0 || vpn >= pageTable.length) {
+                break;
+            }
+            
+            VMKernel.getVMLock().acquire();
+            
+            if (!pageTable[vpn].valid) {
+                VMKernel.getVMLock().release();
+                handlePageFault(vaddr);
+                VMKernel.getVMLock().acquire();
+                
+                if (!pageTable[vpn].valid) {
+                    VMKernel.getVMLock().release();
+                    break;
+                }
+            }
+            
+            if (pageTable[vpn].readOnly) {
+                VMKernel.getVMLock().release();
+                break;
+            }
+            
+            int ppn = pageTable[vpn].ppn;
+            VMKernel.pinPage(ppn);
+            VMKernel.getVMLock().release();
+            
+            int paddr = ppn * pageSize + addrOffset;
+            System.arraycopy(data, offset, memory, paddr, transfer);
+            
+            pageTable[vpn].used = true;
+            pageTable[vpn].dirty = true;
+            
+            VMKernel.unpinPage(ppn);
+            
+            vaddr += transfer;
+            offset += transfer;
+            amount += transfer;
+            length -= transfer;
+        }
+        
+        return amount;
+    }
+    
+    public TranslationEntry getPageTableEntry(int vpn) {
+        if (vpn < 0 || vpn >= pageTable.length) {
+            return null;
+        }
+        return pageTable[vpn];
+    }
+    
+    public void setSwapPage(int vpn, int swapPage) {
+        processLock.acquire();
+        vpnToSwapPage.put(vpn, swapPage);
+        processLock.release();
+    }
+    
+    public boolean hasSwapPage(int vpn) {
+        processLock.acquire();
+        boolean result = vpnToSwapPage.containsKey(vpn);
+        processLock.release();
+        return result;
+    }
+    
+    public int getSwapPage(int vpn) {
+        processLock.acquire();
+        Integer swapPage = vpnToSwapPage.get(vpn);
+        processLock.release();
+        return (swapPage != null) ? swapPage : -1;
+    }
 
-	private static final int pageSize = Processor.pageSize;
-
-	private static final char dbgProcess = 'a';
-
-	private static final char dbgVM = 'v';
+    private static final int pageSize = Processor.pageSize;
+    private static final char dbgProcess = 'a';
+    private static final char dbgVM = 'v';
 }
